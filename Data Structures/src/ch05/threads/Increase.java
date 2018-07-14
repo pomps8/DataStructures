2c993e46fe4b740b9d782fbf8c5ea9c0a9805914
Anthony Pompili <apompz@yahoo.com>
2018-07-14 01:10:03 -0400

    Added increase class
    Takes a counter object and an amount value and increases the count's
    class variable by that amount.

---
+++ Data Structures/src/ch05/threads/Increase.java
@@ -0,0 +1,23 @@
+package ch05.threads;
+
+public class Increase implements Runnable
+{
+
+	private Counter c;
+	private int amount;
+	
+	public Increase(Counter c, int amount)
+	{
+		this.c = c;
+		this.amount = amount;
+	}
+	
+	// Overridden method from Runnable 
+	@Override
+	public void run()
+	{
+		for(int i = 1; i <= amount; i++)
+			c.increment();
+	}
+
+}
