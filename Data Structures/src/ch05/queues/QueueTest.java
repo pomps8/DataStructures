a1491c9974b53e1f7a122985ba5b1b27f8c6059b
Anthony Pompili <apompz@yahoo.com>
2018-07-18 00:41:58 -0400

    Updated old code to use "var" reserved word

--- Data Structures/src/ch05/queues/QueueTest.java
+++ Data Structures/src/ch05/queues/QueueTest.java
@@ -4,7 +4,7 @@ public class QueueTest
 {
 	public static void main(String []args)
 	{
-		BoundedQueueInterface<String> myQueue = new ArrayBoundQueue<String>();
+		var myQueue = new ArrayBoundQueue<String>();
 		try {
 			myQueue.enqueue("Anthony");
 			myQueue.enqueue("Anthony");
