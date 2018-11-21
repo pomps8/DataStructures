d8492d98b6883dd1c54bee6428efa39db8c15e80
Anthony Pompili <apompz@yahoo.com>
2018-11-20 23:56:40 -0500

    Exceptions for priority queue class

---
+++ Data Structures/src/ch09/priorityQueues/PriQOverflowException.java
@@ -0,0 +1,15 @@
+package ch09.priorityQueues;
+
+public class PriQOverflowException extends RuntimeException
+{
+
+	public PriQOverflowException()
+	{
+		super();
+	}
+	
+	public PriQOverflowException(String message)
+	{
+		super(message);
+	}
+}
